af85a8d97d835e6840a85124e1210149ef7909bc
samschmitt30 <chubbypenguin30@gmail.com>
2026-04-02 08:08:48 -0500

    Add files via upload

---
+++ sketch_feb17a.ino
@@ -0,0 +1,29 @@
+//Project 25 - Using interrupts
+
+#define LED 13
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  attachInterrupt(0, displayMicros, RISING);
+  attachInterrupt(1, displayMillis, RISING); 
+}
+void displayMicros()
+{
+  //Serial.write("micros() = ");
+  Serial.println("micros() = :(");
+}
+
+void displayMillis()
+{
+  //Serial.write("millis() = :(");
+  Serial.println("millis() = :(");
+}
+
+void loop() 
+{
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+}
